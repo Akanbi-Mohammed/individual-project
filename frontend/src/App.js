dcff68873ca657a6de00a067d47b5a0788bc4454
Akanbi-Mohammed <mohammedakanbi20@gmail.com>
2025-01-20 17:55:27 +0000

    made more changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,11 @@ import LogIn from './logIn';
 import HomePage from './homePage';
 import SignUp from './signUp';
 import ContactUs from './contactUs';
+import ExpenseTracker from './expenseTracker';
+import GoalsTracker from './GoalTracker'
+import BudgetTracker from './budgetTracker'
+import Analytics from "./analytics"
+
 
 function App() {
   return (
@@ -18,6 +23,10 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/expenses" element={<ExpenseTracker />} />
+            <Route path="/goals" element={<GoalsTracker/>} />
+            <Route path="/budgets" element={<BudgetTracker/>} />
+            <Route path="/analytics" element={<Analytics/>} />
         </Routes>
       </Router>
   );
